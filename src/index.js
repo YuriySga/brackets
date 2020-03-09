2bef71fccb08c766bb020fc667cf9e31b8ff1b56
YuriySga <ess1928@yandex.ru>
2020-03-10 00:29:23 +0400

    feat: update brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) {  
+   arr = str.split(''); 
+   let brConf = bracketsConfig;  
+ 
+   brConf.forEach(itemAr => deleteBr(arr, itemAr));  
+   brConf.forEach(itemAr => deleteBr(arr, itemAr));
+   brConf.forEach(itemAr => deleteBr(arr, itemAr));
+   brConf.forEach(itemAr => deleteBr(arr, itemAr));
+   brConf.forEach(itemAr => deleteBr(arr, itemAr));
+
+   return (arr.length == 0) ? true : false;  
+
+    function deleteBr (ar, bc) {        
+        let len = ar.length;
+        for (let i = 0; i < ar.length-1; i++) { 
+          if (ar[i] == bc[0] && ar[i+1] == bc[1]) {  
+               ar.splice(i+1, 1);  //delete
+               ar.splice(i, 1);     //delete         
+          } else {
+            //nop;
+          }
+        } 
+    (len == ar.length) ? arr = ar : deleteBr(ar, bc); 
+    }
 }
