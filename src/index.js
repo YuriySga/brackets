cea515b0b59f38f8ce2e1040e53ae16e52d8ef3f
YuriySga <ess1928@yandex.ru>
2020-03-10 23:32:55 +0400

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
-module.exports = function check(str, bracketsConfig) {  
-   arr = str.split(''); 
-   let brConf = bracketsConfig;  
- 
-   brConf.forEach(itemAr => deleteBr(arr, itemAr));  
-   brConf.forEach(itemAr => deleteBr(arr, itemAr));
-   brConf.forEach(itemAr => deleteBr(arr, itemAr));
-   brConf.forEach(itemAr => deleteBr(arr, itemAr));
-   brConf.forEach(itemAr => deleteBr(arr, itemAr));
+module.exports = function check(str, bracketsConfig) {
+  
+  if (typeof str == "string") {   //если первый прогон
+    str = str.split('');
+  }     
 
-   return (arr.length == 0) ? true : false;  
-
-    function deleteBr (ar, bc) {        
-        let len = ar.length;
-        for (let i = 0; i < ar.length-1; i++) { 
-          if (ar[i] == bc[0] && ar[i+1] == bc[1]) {  
-               ar.splice(i+1, 1);  //delete
-               ar.splice(i, 1);     //delete         
-          } else {
-            //nop;
-          }
-        } 
-    (len == ar.length) ? arr = ar : deleteBr(ar, bc); 
+  let len = str.length;
+  bracketsConfig.forEach(itemAr => {
+    for (let i = 0; i < str.length-1; i++) { 
+      if (str[i] == itemAr[0] && str[i+1] == itemAr[1]) {  
+        str.splice(i+1, 1);  //delete
+        str.splice(i, 1);     //delete         
+      }
     }
+  });
+
+  return (len == str.length) ? (str.length == 0) ? true : false : check(str, bracketsConfig); 
 }
+
+
